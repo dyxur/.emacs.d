f8411c1e497357afde4a0be8b8ebf03c1e98cbb7
Dylan Muchenberger <dylan.muchenberger@gmail.com>
2021-06-24 12:45:48 +0200

    Updated init

--- init.el
+++ init.el
@@ -1,9 +1,21 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+(setq package-list '(lua-mode dracula-theme))
+
 (package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq c-basic-offset 4)
 (setq-default indent-line-function 'insert-tab)
+(global-whitespace-mode 1)
+
+(setq-default lua-indent-level 4)
