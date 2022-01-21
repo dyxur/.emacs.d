63a62c737345b4f712892524beda388ff47ba58f
WhitestYakumo <dylan.muchenberger@gmail.com>
2022-01-21 12:32:06 +0100

    Small update to my emacs init

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   '("824d07981667fd7d63488756b6d6a4036bae972d26337babf7b56df6e42f2bcd" default))
+   '("b4ba3e1bba2e303265eb3e9753215408e75e031f7c894786ad04cabef46ff94c" "824d07981667fd7d63488756b6d6a4036bae972d26337babf7b56df6e42f2bcd" default))
  '(package-selected-packages '(luarocks lsp-mode dracula-theme 2048-game lua-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -24,9 +24,15 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; =================================
+;; || EMACS CONFIGURATION          ||
+;; =================================
+
+;; Add the MELPA archive
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+;; Aquire and process needed Packages
 (setq package-list '(lua-mode dracula-theme))
 
 (package-initialize)
@@ -38,13 +44,48 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+
+;; Setup indetation
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq c-basic-offset 4)
 (setq-default indent-line-function 'insert-tab)
+;; Indentation for lua-mode
+(setq-default lua-indent-level 4)
+
+
+;; Whitesapce
 (global-whitespace-mode 1)
 (global-display-line-numbers-mode 1)
-
-(setq-default lua-indent-level 4)
-(desktop-save-mode 1)
 (setq-default whitespace-line-column 250)
+
+;; Turn on saving of session
+(desktop-save-mode 1)
+
+;; Duplicate line down
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank)
+  )
+
+;; Delete selection
+(delete-selection-mode 1)
+
+;; Keyboard shortcuts
+(global-set-key (kbd "C-c <C-down>") 'duplicate-line)
+(global-set-key (kbd "C-c f") 'forward-whitespace)
+
+
+;; =================================
+;; || ORG-MODE CONFIGURATION       ||
+;; =================================
+
+;; Enable Org mode
+(require 'org)
+;; Use org mode by default with files ending in ".org"
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
